19c33dc96a33fe4f5ef3976e620313ecfdf9a83e
King <king.yu.tian.work@outlook.com>
2023-05-05 11:44:07 +0000

    Add api_gateway framework

---
+++ api_gateway/src/api_gateway.rs
@@ -0,0 +1,35 @@
+use tonic::{Request, Response, Status};
+
+pub mod gateway {
+    tonic::include_proto!("gateway");
+}
+
+// use gateway::{ProcessImageRequest, ProcessImageResponse, ProcessingType};
+use gateway::{ProcessImageRequest, ProcessImageResponse};
+
+pub struct ApiGateway {}
+
+impl Default for ApiGateway {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+#[tonic::async_trait]
+impl gateway::api_gateway_server::ApiGateway for ApiGateway {
+    async fn process_image(
+        &self,
+        request: Request<ProcessImageRequest>,
+    ) -> Result<Response<ProcessImageResponse>, Status> {
+        let data = request.into_inner();
+        let processing_type = data.processing_type;
+        let image_url = data.image_url;
+
+        // Implement logic to call the appropriate image processing service based on the processing_type value.
+        // You can use the tonic client to call the services.
+
+        Ok(Response::new(ProcessImageResponse {
+            result: "Image processed".to_string(),
+        }))
+    }
+}
--- api_gateway/src/main.rs
+++ api_gateway/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::transport::Server;
+
+mod api_gateway;
+use api_gateway::gateway;
+use api_gateway::ApiGateway;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50051".parse().unwrap();
+    let api_gateway = ApiGateway::default();
+
+    Server::builder()
+        .add_service(gateway::api_gateway_server::ApiGatewayServer::new(
+            api_gateway,
+        ))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
