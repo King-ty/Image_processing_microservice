fb7e9482947bc1d0aa26b3969496fb92f655c246
King <king.yu.tian.work@outlook.com>
2023-05-06 10:22:24 +0000

    Add pixelate service to api gateway

--- server/api_gateway/src/api_gateway.rs
+++ server/api_gateway/src/api_gateway.rs
@@ -10,22 +10,37 @@ pub mod image_processing {
 
 // use gateway::{ProcessImageRequest, ProcessImageResponse, ProcessingType};
 use gateway::{ProcessImageRequest, ProcessImageResponse, ProcessingType};
-use image_processing::grayscale_service_client::GrayscaleServiceClient;
 use image_processing::ImageRequest;
+use image_processing::{
+    grayscale_service_client::GrayscaleServiceClient,
+    pixelate_service_client::PixelateServiceClient,
+};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tonic::transport::Channel;
 
 pub struct ApiGatewayImpl {
     grayscale_client: Arc<Mutex<GrayscaleServiceClient<Channel>>>,
+    pixelate_client: Arc<Mutex<PixelateServiceClient<Channel>>>,
 }
 
 impl ApiGatewayImpl {
-    pub async fn new(grayscale_service_addr: String) -> Result<Self, Box<dyn std::error::Error>> {
-        let grayscale_client = GrayscaleServiceClient::connect(grayscale_service_addr).await?;
-        let grayscale_client = Arc::new(Mutex::new(grayscale_client));
+    pub async fn new(
+        grayscale_service_addr: String,
+        pixelate_service_addr: String,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let grayscale_client = Arc::new(Mutex::new(
+            GrayscaleServiceClient::connect(grayscale_service_addr).await?,
+        ));
 
-        Ok(Self { grayscale_client })
+        let pixelate_client = Arc::new(Mutex::new(
+            PixelateServiceClient::connect(pixelate_service_addr).await?,
+        ));
+
+        Ok(Self {
+            grayscale_client,
+            pixelate_client,
+        })
     }
 }
 
@@ -39,18 +54,26 @@ impl gateway::api_gateway_server::ApiGateway for ApiGatewayImpl {
         let processing_type = data.processing_type;
         let image_data = data.image_data;
 
+        let process_request = ImageRequest { image_data };
+
         // Implement logic to call the appropriate image processing service based on the processing_type value.
         let result = match ProcessingType::from_i32(processing_type).unwrap() {
             ProcessingType::Grayscale => {
-                let grayscale_request = ImageRequest { image_data };
                 let mut grayscale_client = self.grayscale_client.lock().await;
                 let grayscale_response = grayscale_client
-                    .grayscale_image(grayscale_request)
+                    .grayscale_image(process_request)
                     .await
                     .map_err(|e| Status::internal(format!("Grayscale service error: {}", e)))?;
                 grayscale_response.into_inner().image_data
             }
-            _ => return Err(Status::unimplemented("Processing type not implemented")),
+            ProcessingType::Pixelate => {
+                let mut pixelate_client = self.pixelate_client.lock().await;
+                let pixelate_response = pixelate_client
+                    .pixelate_image(process_request)
+                    .await
+                    .map_err(|e| Status::internal(format!("Pixelate service error: {}", e)))?;
+                pixelate_response.into_inner().image_data
+            }
         };
 
         Ok(Response::new(ProcessImageResponse { result }))
--- server/api_gateway/src/main.rs
+++ server/api_gateway/src/main.rs
@@ -7,8 +7,9 @@ use api_gateway::ApiGatewayImpl;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let grayscale_service_addr = "http://[::1]:50052";
-    let api_gateway = ApiGatewayImpl::new(grayscale_service_addr.to_string()).await?;
+    let grayscale_service_addr = "http://[::1]:50052".to_string();
+    let pixelate_service_addr = "http://[::1]:50053".to_string();
+    let api_gateway = ApiGatewayImpl::new(grayscale_service_addr, pixelate_service_addr).await?;
 
     Server::builder()
         .add_service(gateway::api_gateway_server::ApiGatewayServer::new(
