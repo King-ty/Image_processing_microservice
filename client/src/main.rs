c319efecae7a71456ba32b54dba91bbb17d31f20
King <king.yu.tian.work@outlook.com>
2023-05-06 10:22:38 +0000

    Add pixelate test to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,7 +4,7 @@ use api::{ProcessImageRequest, ProcessingType};
 use std::env;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
+// use std::path::Path;
 // use tonic::transport::Channel;
 
 pub mod api {
@@ -15,7 +15,7 @@ pub mod api {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_addr = env::var("API_GATEWAY_ADDR").unwrap_or("http://[::1]:50051".to_string());
     let image_path = env::var("IMAGE_PATH").unwrap_or("test.jpg".to_string());
-    let output_path = env::var("OUTPUT_PATH").unwrap_or("output.jpg".to_string());
+    let output_path = env::var("OUTPUT_PATH").unwrap_or("./output".to_string());
 
     let mut file = File::open(image_path)?;
     let mut image_data = Vec::new();
@@ -23,21 +23,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = ApiGatewayClient::connect(server_addr).await?;
 
-    let request = ProcessImageRequest {
-        image_data,
-        processing_type: ProcessingType::Grayscale as i32,
-    };
+    // test Grayscale
+    if true {
+        let request = ProcessImageRequest {
+            image_data: image_data.clone(),
+            processing_type: ProcessingType::Grayscale as i32,
+        };
 
-    let response = client.process_image(request).await?.into_inner();
+        let response = client.process_image(request).await?.into_inner();
 
-    let processed_image_data = response.result;
+        let processed_image_data = response.result;
 
-    // Save the processed image
-    let output_path = Path::new(&output_path);
-    let mut output_file = File::create(output_path)?;
-    output_file.write_all(&processed_image_data)?;
+        // Save the processed image
+        let output_path = format!("{}-grayscale.jpg", &output_path);
+        let mut output_file = File::create(&output_path)?;
+        output_file.write_all(&processed_image_data)?;
 
-    println!("Processed image saved to {:?}", output_path);
+        println!("Grayscaled image saved to {:?}", output_path);
+    }
+
+    // test Pixelate
+    if true {
+        let request = ProcessImageRequest {
+            image_data: image_data.clone(),
+            processing_type: ProcessingType::Pixelate as i32,
+        };
+
+        let response = client.process_image(request).await?.into_inner();
+
+        let processed_image_data = response.result;
+
+        // Save the processed image
+        let output_path = format!("{}-pixelate.jpg", &output_path);
+        let mut output_file = File::create(&output_path)?;
+        output_file.write_all(&processed_image_data)?;
+
+        println!("Pixelated image saved to {:?}", output_path);
+    }
 
     Ok(())
 }
