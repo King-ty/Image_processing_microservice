c0b05ed8ffd2c6cb1e0a7d0b7338ea9d90438033
King <king.yu.tian.work@outlook.com>
2023-05-06 10:21:57 +0000

    Finish pixel_service

--- server/pixelate_service/src/main.rs
+++ server/pixelate_service/src/main.rs
@@ -1,19 +1,17 @@
 use tonic::transport::Server;
 
 mod pixelate_service;
-use pixelate_service::PixelateService;
+use pixelate_service::PixelateServiceImpl;
 
-use pixelate_service::image_processing;
+use pixelate_service::image_processing::pixelate_service_server::PixelateServiceServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:50053".parse().unwrap();
-    let pixelate_service = PixelateService::default();
+    let addr = "[::1]:50053".parse()?;
+    let pixelate_service = PixelateServiceImpl::default();
 
     Server::builder()
-        .add_service(
-            image_processing::pixelate_service_server::PixelateServiceServer::new(pixelate_service),
-        )
+        .add_service(PixelateServiceServer::new(pixelate_service))
         .serve(addr)
         .await?;
 
--- server/pixelate_service/src/pixelate_service.rs
+++ server/pixelate_service/src/pixelate_service.rs
@@ -1,32 +1,52 @@
+use image::ImageFormat;
+use image_processing::pixelate_service_server::PixelateService;
+use image_processing::{ImageRequest, ImageResponse};
+use std::io::Cursor;
 use tonic::{Request, Response, Status};
 
 pub mod image_processing {
     tonic::include_proto!("image_processing");
 }
 
-use image_processing::{ImageUrl, ProcessingResult};
-
-pub struct PixelateService {}
-
-impl Default for PixelateService {
-    fn default() -> Self {
-        Self {}
-    }
-}
+#[derive(Default)]
+pub struct PixelateServiceImpl;
 
 #[tonic::async_trait]
-impl image_processing::pixelate_service_server::PixelateService for PixelateService {
-    async fn process_image(
+impl PixelateService for PixelateServiceImpl {
+    async fn pixelate_image(
         &self,
-        request: Request<ImageUrl>,
-    ) -> Result<Response<ProcessingResult>, Status> {
-        let image_url = request.into_inner().image_url;
+        request: Request<ImageRequest>,
+    ) -> Result<Response<ImageResponse>, Status> {
+        let req = request.into_inner();
 
-        // Implement the logic to pixelate the image.
-        // You can use the 'image' crate to perform the pixelation.
+        // Implement the logic to convert the image to grayscale.
+        let img = match image::load_from_memory(&req.image_data) {
+            Ok(img) => img,
+            Err(_) => {
+                return Err(Status::invalid_argument("Invalid image data or format"));
+            }
+        };
 
-        Ok(Response::new(ProcessingResult {
-            result: "Image pixelated".to_string(),
-        }))
+        let (width, height) = (img.width(), img.height());
+        let pixelated_image = img
+            .resize(
+                width / 10,
+                height / 10,
+                image::imageops::FilterType::Nearest,
+            )
+            .resize(width, height, image::imageops::FilterType::Nearest);
+
+        let mut buffer = Cursor::new(Vec::new());
+        if let Err(_) = pixelated_image.write_to(&mut buffer, ImageFormat::Jpeg) {
+            return Err(Status::internal(
+                "Failed to write grayscale image to buffer",
+            ));
+        }
+
+        let resp = ImageResponse {
+            image_data: buffer.into_inner(),
+        };
+
+        Ok(Response::new(resp))
     }
 }
