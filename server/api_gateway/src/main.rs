0c45711dff79b9b65cd10ee122f850c1c0e7100f
King <king.yu.tian.work@outlook.com>
2023-05-19 07:10:08 +0000

    Add Config to api gateway

--- server/api_gateway/src/main.rs
+++ server/api_gateway/src/main.rs
@@ -1,3 +1,4 @@
+use config::Config;
 use tonic::transport::Server;
 
 mod api_gateway;
@@ -6,12 +7,35 @@ use api_gateway::ApiGatewayImpl;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let grayscale_service_addr = "http://[::1]:50052".to_string();
-    let pixelate_service_addr = "http://[::1]:50053".to_string();
-    let blur_service_addr = "http://[::1]:50054".to_string();
-    let ascii_service_addr = "http://[::1]:50056".to_string();
-    let resize_service_addr = "http://[::1]:50057".to_string();
+    let config = Config::builder()
+        .add_source(config::File::with_name("Config"))
+        .build()?;
+    let addr = config.get("addr").unwrap_or("[::]:50051".parse()?);
+    // let addr = "[::1]:50051".parse()?;
+    println!("Listening on: {}", addr); // Debug
+
+    // let grayscale_service_addr = "http://[::1]:50052".to_string();
+    // let pixelate_service_addr = "http://[::1]:50053".to_string();
+    // let blur_service_addr = "http://[::1]:50054".to_string();
+    // let ascii_service_addr = "http://[::1]:50056".to_string();
+    // let resize_service_addr = "http://[::1]:50057".to_string();
+
+    let grayscale_service_addr = config
+        .get("grayscale_service_addr")
+        .unwrap_or("http://[::1]:50052".to_string());
+    let pixelate_service_addr = config
+        .get("pixelate_service_addr")
+        .unwrap_or("http://[::1]:50053".to_string());
+    let blur_service_addr = config
+        .get("blur_service_addr")
+        .unwrap_or("http://[::1]:50054".to_string());
+    let ascii_service_addr = config
+        .get("ascii_service_addr")
+        .unwrap_or("http://[::1]:50056".to_string());
+    let resize_service_addr = config
+        .get("resize_service_addr")
+        .unwrap_or("http://[::1]:50057".to_string());
+
     // Add new addr here
 
     let api_gateway = ApiGatewayImpl::new(
