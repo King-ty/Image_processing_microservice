65432aa335bac550eedfb91cb6a9f4d701f59275
King <king.yu.tian.work@outlook.com>
2023-05-23 15:31:27 +0000

    Add tls and token to pixelate service

--- server/pixelate_service/src/main.rs
+++ server/pixelate_service/src/main.rs
@@ -1,5 +1,9 @@
+mod middleware;
+use middleware::check_auth;
+
 use config::Config;
-use tonic::transport::Server;
+use std::time::Duration;
+use tonic::transport::{Certificate, Identity, Server, ServerTlsConfig};
 
 mod pixelate_service;
 use pixelate_service::PixelateServiceImpl;
@@ -12,11 +16,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_source(config::File::with_name("Config"))
         .build()?;
     let addr = config.get("addr").unwrap_or("[::1]:50053".parse()?);
+
+    // 配置tls
+    let cert_dir = config
+        .get("cert_dir")
+        .unwrap_or("./tls/local.pem".to_string());
+    let key_dir = config
+        .get("key_dir")
+        .unwrap_or("./tls/local.key".to_string());
+    let client_ca_dir = config
+        .get("client_ca_dir")
+        .unwrap_or("./tls/client_ca.pem".to_string());
+
+    let cert = std::fs::read_to_string(cert_dir)?;
+    let key = std::fs::read_to_string(key_dir)?;
+    let server_identity = Identity::from_pem(cert, key);
+
+    let client_ca_cert = std::fs::read_to_string(client_ca_dir)?;
+    let client_ca_cert = Certificate::from_pem(client_ca_cert);
+    let tls = ServerTlsConfig::new()
+        .identity(server_identity)
+        .client_ca_root(client_ca_cert);
+
+    // 配置中间件
+    let layer = tower::ServiceBuilder::new()
+        .timeout(Duration::from_micros(1000))
+        .layer(tonic::service::interceptor(check_auth))
+        .into_inner();
+
     let pixelate_service = PixelateServiceImpl::default();
 
     println!("Listening on: {}", addr); // Debug
 
     Server::builder()
+        .tls_config(tls)?
+        .layer(layer)
         .add_service(PixelateServiceServer::new(pixelate_service))
         .serve(addr)
         .await?;
